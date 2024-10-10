0acb8b761e014bc187df9518b8bc0acc3411a076
kulwinder <kulwinderkaurit@gmail.com>
2024-10-10 09:56:26 -0600

    update

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,36 @@
+"use strict";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Kulwinder Kaur
+   Date: 2024-10-08
 
 */
 
+/* Display date & time */
+
+document.getElementById("dateNow").innerHTML = `m/d/y/ <br /> h:m:s`;
+
+/* Display the time left until New Years EVE */
+
+document.getElementById("days").textContent= "dd";
+document.getElementById("hrs").textContent= "hh";
+document.getElementById("mins").textContent= "mm";
+document.getElementById("secs").textContent= "ss";
+
+/* store the current date and time */
+
+var currentDay = new Date("May, 23 , 2018  14:35:05"); 
+var dateStr = currentDay.toLocaleDateString();
+var timeeStr = currentDay.toLocaleTimeString();
+
+
+
+/* Display date & time */
+
+document.getElementById("dateNow").innerHTML =
+dateStr + "<br/>" + timeeStr;
